88493196652913ba10e91faaa21fbb6d6495b9d0
Dhruv Chavda <dhruvchavda@192.168.1.12>
2024-04-27 11:21:17 +0530

    Util code

--- src/index.js
+++ src/index.js
@@ -7,8 +7,14 @@ dotenv.config({
 
 
 connectDB()
-
-
+.then(()=>{
+    app.listen(process.env.PORT||8000,()=>{
+        console.log(`Server is running at port ${process.env.PORT}`);
+    })
+})
+.catch((error)=>{
+    console.log("MongoDB connection failed ",error)
+})
 
 
 
---
+++ src/utils/ApiResponse.js
@@ -0,0 +1,13 @@
+class ApiResponse {
+    constructor(statuscode ,data ,message = "Success"){
+        this.statusCode=statuscode
+        this.data=data
+        this.message=message
+        this.success = statuscode < 400
+        // Informational responses(100 - 199)
+        // Successful responses(200 - 299)
+        // Redirection messages(300 - 399)
+        // Client error responses(400 - 499)
+        // Server error responses(500 - 599)
+    }
+}
