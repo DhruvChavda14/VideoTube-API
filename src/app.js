1ce7b35f1567679cf845b354e801cf18f515b06c
Dhruv Chavda <dhruvchavda@192.168.1.6>
2024-04-29 17:25:07 +0530

    all setup configured

--- src/app.js
+++ src/app.js
@@ -17,4 +17,11 @@ app.use(express.urlencoded({extended : true,
 app.use(express.static("public"))
 app.use(cookieParser())
 
+//router import
+import userRouter from "./routes/user.routes.js"
+
+//routes declaration
+app.use("/api/v1/users",userRouter)
+
+
 export { app }
